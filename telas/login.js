d1bf7640cfa11e53069bac46935890a420adfa06
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-01-02 22:54:04 -0300

    Create login.js

---
+++ telas/login.js
@@ -0,0 +1,9 @@
+function validarCampos(){
+    var email = document.getElementById('editTextTextEmailAddress')
+    var senha = document.getElementById('editTextTextPassword')
+        
+        if (email == null || senha == null) {
+            window.alert("Insira as informações faltantes")
+            return false    
+        }
+}
